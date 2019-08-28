7e7f04f3ba543f31b5d92f04d80403feb6748690
malik <malik.benhebri@gmail.com>
2019-08-28 12:47:53 +0200

    Get et Set de DEFINITION

--- projet/src/Entity/Definition.php
+++ projet/src/Entity/Definition.php
@@ -72,4 +72,148 @@ class Definition
     
 
 
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param  int  $id
+     *
+     * @return  self
+     */ 
+    public function setId(int $id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     *
+     * @return  string
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @param  string  $description
+     *
+     * @return  self
+     */ 
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of terme
+     *
+     * @return  string
+     */ 
+    public function getTerme()
+    {
+        return $this->terme;
+    }
+
+    /**
+     * Set the value of terme
+     *
+     * @param  string  $terme
+     *
+     * @return  self
+     */ 
+    public function setTerme(string $terme)
+    {
+        $this->terme = $terme;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of photo
+     *
+     * @return  string|null
+     */ 
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set the value of photo
+     *
+     * @param  string|null  $photo
+     *
+     * @return  self
+     */ 
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of videoUrl
+     *
+     * @return  string|null
+     */ 
+    public function getVideoUrl()
+    {
+        return $this->videoUrl;
+    }
+
+    /**
+     * Set the value of videoUrl
+     *
+     * @param  string|null  $videoUrl
+     *
+     * @return  self
+     */ 
+    public function setVideoUrl($videoUrl)
+    {
+        $this->videoUrl = $videoUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of videoUpload
+     *
+     * @return  string|null
+     */ 
+    public function getVideoUpload()
+    {
+        return $this->videoUpload;
+    }
+
+    /**
+     * Set the value of videoUpload
+     *
+     * @param  string|null  $videoUpload
+     *
+     * @return  self
+     */ 
+    public function setVideoUpload($videoUpload)
+    {
+        $this->videoUpload = $videoUpload;
+
+        return $this;
+    }
 }
