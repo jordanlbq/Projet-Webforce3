712a2ba4d25390ecee4791f7955f7598474c949c
ismael <ismail78@hotmail.com>
2019-09-05 12:31:09 +0200

    Page a propos, but du site, cgu, qui sommes nous, mention légales

--- projet/src/Controller/BaseController.php
+++ projet/src/Controller/BaseController.php
@@ -18,4 +18,34 @@ class BaseController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route ("/cgu", name="CGU")
+     * 
+     */
+    public function CGU(){
+        return $this->render('base/cgu.html.twig',[
+
+        ]);
+    }
+
+    /**
+     * @Route ("/apropos", name="A Propos")
+     * 
+     */
+    public function apropos(){
+        return $this->render('base/a_propos.html.twig',[
+
+        ]);
+    }
+
+    /**
+     * @Route ("/quisommesnous", name="Qui Sommes Nous")
+     * 
+     */
+    public function quisommesnous(){
+        return $this->render('base/qui_sommes_nous.html.twig',[
+
+        ]);
+    }
 }
